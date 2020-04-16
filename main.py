47fd354b6639abe35f2f95b7cd761d2ddbb32242
fanu1k <golenev.nickita@gmail.com>
2020-04-16 20:14:47 +0300

    Логика выбора уровня сложности

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import telebot
-import subprocess as sp
 from telebot import types
-
 
 bot = telebot.TeleBot('1265769270:AAEW4OJS-8ZvCB272gtHCSx_RLwids1llcc')
 
@@ -11,9 +9,10 @@ def start_message(message):
     if message.chat.id not in "base":
         bot.send_message(message.chat.id, 'Приветствие')
         msg = bot.send_message(message.chat.id, 'Введите предпочитаемое имя пользователя')
-        register_next_step_handler(msg, register)
+        bot.register_next_step_handler(msg, register)
     else:
         bot.send_message(message.chat.id, 'Вы уже зарегестрированны')
+
 
 def register(message):
     chat_id = message.chat.id
@@ -22,8 +21,22 @@ def register(message):
         msg = bot.send_message(chat_id, 'incorrect login')
         bot.register_next_step_handler(msg, register)
         return
-    #Логин в базу
+    # Логин в базу
     msg = bot.send_message(chat_id, 'Добро пожаловть ' + text)
+    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    button_pay = types.KeyboardButton(text="Пройти тест")
+    button_file = types.KeyboardButton(text="Начать с нуля")
+    keyboard.add(button_pay, button_file)
+    msg = bot.send_message(message.chat.id, "Вы можете пройти тест на знание python, либо начать с начала", reply_markup=keyboard)
+    bot.register_next_step_handler(msg, choise)
+
+
+def choise(message):
+    if message.text == 'Пройти тест':
+        # Тест
+    if message.text == 'Начать с нуля':
+        # Игра
+
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
