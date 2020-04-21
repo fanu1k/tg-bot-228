75ecccf5ff8dc0786095946d179379271a3cde6c
fanu1k <golenev.nickita@gmail.com>
2020-04-21 20:26:29 +0300

    bot minor update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import telebot
 from telebot import types
+from data import *
 
 bot = telebot.TeleBot('1265769270:AAEW4OJS-8ZvCB272gtHCSx_RLwids1llcc')
 
@@ -17,11 +18,11 @@ def start_message(message):
 def register(message):
     chat_id = message.chat.id
     text = message.text
-    if not '''проверки логина''':
+    if not (5 < len(text) < 15):
         msg = bot.send_message(chat_id, 'incorrect login')
         bot.register_next_step_handler(msg, register)
         return
-    # Логин в базу (ЕГОР!!!!!!!!!)
+    insert_name(chat_id, text)
     msg = bot.send_message(chat_id, 'Добро пожаловть ' + text)
     keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
     button_pay = types.KeyboardButton(text="Пройти тест")
@@ -40,16 +41,13 @@ def choise(message):
     elif message.text == 'Начать с нуля':
         keyboard = types.ReplyKeyboardRemove()
         bot.send_message(message.chat_id, 'txt', reply_markup=keyboard)
+        change_exp(message.chat_id, 0)
         game(message)
     else:
         bot.register_next_step_handler(message, choise)
 
 
 def test(message):
-    pass
-
-
-def game(message, lvl=0):
     pass
 
 
