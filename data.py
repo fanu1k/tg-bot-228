d243d21b01334ee0c118b43a2dc4e825091577ff
krestonoseu <evgeny.yatsuro@yandex.ru>
2020-04-22 21:16:31 +0400

    преобразование файла работы с базой данных + изменение самой базы

--- data.py
+++ data.py
@@ -1,82 +1,54 @@
 import sqlite3
 
 
-<<<<<<< Updated upstream
-def insert_name(num, string):
-    con = sqlite3.connect('user_database.db')
-    cur = con.cursor()
-    result = cur.execute('''SELECT Name FROM database''').fetchall()
-    for element in result:
-        if string in element:
+class User:
+    def __init__(self, id, login, exp, complited):
+        self.id = id
+        self.login = login
+        self.exp = exp
+        self.complited = complited
+
+    def insert_name(self):
+        con = sqlite3.connect('user_database.db')
+        cur = con.cursor()
+        result = cur.execute('''SELECT Login FROM database''').fetchall()
+        for element in result:
+            if self.login in element:
+                con.close()
+                return "имя занято"
+        else:
+            cur.execute(
+                '''INSERT INTO database (ID, Login, Exp, Completed) VALUES (?, ?, 0, '0/')''', (self.id, self.login))
+            con.commit()
             con.close()
-            return "имя занято"
-    else:
-        cur.execute(
-            '''INSERT INTO database (ID, Name, Exp, Completed) VALUES (?, ?, 0, '0/')''', (num, string))
-=======
-def insert_name(id, name):
-    con = sqlite3.connect('user_database.db')
-    cur = con.cursor()
-    result = cur.execute('''SELECT Name FROM database''').fetchall()
-    if name in result:
-        con.close()
-        return "имя занято"
-    else:
-        cur.execute('''INSERT INTO database(ID, Name, Exp, Completed) VALUES(id, name, 0, 0)''')
->>>>>>> Stashed changes
+
+    def change_exp(self, num):
+        con = sqlite3.connect('user_database.db')
+        cur = con.cursor()
+        result = cur.execute(
+            '''SELECT Exp FROM database WHERE Login = ?''', self.login).fetchone()
+        self.exp = int(result[0]) + num
+        cur.execute('''UPDATE database SET Exp = ? WHERE Login = ?''',
+                    (self.exp, self.login))
         con.commit()
         con.close()
 
+    def change_completed(self, exer):
+        con = sqlite3.connect('user_database.db')
+        cur = con.cursor()
+        result = cur.execute(
+            '''SELECT Completed FROM database WHERE Login = ?''', self.login).fetchone()
+        self.complited = str(result[0]) + str(exer) + '/'
+        cur.execute('''UPDATE database SET Completed = ? WHERE Login = ?''',
+                    (self.complited, self.login))
+        con.commit()
+        con.close()
 
-def change_exp(id, num):
-<<<<<<< Updated upstream
-    con = sqlite3.connect('user_database.db')
-    cur = con.cursor()
-    result = cur.execute(
-        '''SELECT Exp FROM database WHERE ID = ?''', (id,)).fetchone()
-    cur.execute('''UPDATE database SET Exp = ? WHERE ID = ?''',
-                (int(result[0]) + num, id))
-=======
-    con = sqlite3.connect('database.db')
-    cur = con.cursor()
-    result = cur.execute('''SELECT Exp FROM database WHERE ID = ?''', (id,)).fetchone()
-    cur.execute('''UPDATE database SET Exp = ? WHERE ID = ?''', (int(result[0]) + num, id))
->>>>>>> Stashed changes
-    con.commit()
-    con.close()
-
-
-def change_completed(id, exer):
-<<<<<<< Updated upstream
-    con = sqlite3.connect('user_database.db')
-    cur = con.cursor()
-    result = cur.execute(
-        '''SELECT Completed FROM database WHERE ID = ?''', (id,)).fetchone()
-    cur.execute('''UPDATE database SET Completed = ? WHERE ID = ?''',
-                (str(result[0]) + str(exer) + '/', id))
-=======
-    con = sqlite3.connect('database.db')
-    cur = con.cursor()
-    result = cur.execute('''SELECT Completed FROM database WHERE ID = ?''', (id,)).fetchone()
-    cur.execute('''UPDATE database SET Completed = ? WHERE ID = ?''', (str(result[0]) + str(exer), id))
->>>>>>> Stashed changes
-    con.commit()
-    con.close()
-
-
-def get_info(id):
-<<<<<<< Updated upstream
-    con = sqlite3.connect('user_database.db')
-    cur = con.cursor()
-    result = cur.execute(
-        '''SELECT * FROM database WHERE ID = ?''', (id,)).fetchone()
-    con.close()
-    ans = [result[1], result[2], result[3].split('/')[:-1]]
-    return ans
-=======
-    con = sqlite3.connect('database.db')
-    cur = con.cursor()
-    result = cur.execute('''SELECT * FROM database WHERE ID = ?''', (id,)).fetchone()
-    con.close()
-    return result
->>>>>>> Stashed changes
+    def get_info(self, id):
+        con = sqlite3.connect('user_database.db')
+        cur = con.cursor()
+        result = cur.execute(
+            '''SELECT * FROM database WHERE Login = ?''', self.login).fetchone()
+        con.close()
+        ans = [result[1], result[2], result[3].split('/')[:-1]]
+        return ans
